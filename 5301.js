109cc4cb31c26536b8f26d42d8d6b81e1911bbbd
James Burgess <jamesmillerburgess@gmail.com>
2017-07-27 23:02:41 +0200

    Added results in comments

--- 5301.js
+++ 5301.js
@@ -22,28 +22,28 @@ if (Meteor.isServer) {
       num++;
     };
 
-    test('c.find({}).fetch()', c.find({}).fetch());
-    test('c.findOne({})', c.findOne({}));
-    test('c.findOne({ a: noop })', c.findOne({ a: noop }));
-    test('c.findOne({ b: noop })', c.findOne({ b: noop }));
-    test('c.findOne({ c: noop })', c.findOne({ c: noop }));
-    test(
+    test('c.find({}).fetch()', c.find({}).fetch());         // 1 both
+    test('c.findOne({})', c.findOne({}));                   // 2 first
+    test('c.findOne({ a: noop })', c.findOne({ a: noop })); // 3 first
+    test('c.findOne({ b: noop })', c.findOne({ b: noop })); // 4 first
+    test('c.findOne({ c: noop })', c.findOne({ c: noop })); // 5 first
+    test(                                                   // 6 first
       'c.findOne({ a: "abc", b: noop })',
       c.findOne({ a: "abc", b: noop }),
     );
-    test(
+    test(                                                   // 7 first
       'c.findOne({ b: noop, a: "abc" })',
       c.findOne({ b: noop, a: "abc" }),
     );
 
-    test('c.find({ a: noop })', c.find({ a: noop }).fetch());
-    test('c.find({ b: noop })', c.find({ b: noop }).fetch());
-    test('c.find({ c: noop })', c.find({ c: noop }).fetch());
-    test(
+    test('c.find({ a: noop })', c.find({ a: noop }).fetch()); // 8  both
+    test('c.find({ b: noop })', c.find({ b: noop }).fetch()); // 9  both
+    test('c.find({ c: noop })', c.find({ c: noop }).fetch()); // 10 both
+    test(                                                     // 11 first
       'c.find({ a: "abc", b: noop })',
       c.find({ a: "abc", b: noop }).fetch(),
     );
-    test(
+    test(                                                     // 12 first
       'c.find({ b: noop, a: "abc" })',
       c.find({ b: noop, a: "abc" }).fetch(),
     );
@@ -66,18 +66,18 @@ if (Meteor.isClient) {
       }
       num++;
     };
-    check(c.find({}).fetch());
-    check(c.findOne({}));
-    check(c.findOne({ a: noop }));           // c: undefined
-    check(c.findOne({ b: noop }));           // c: undefined
-    check(c.findOne({ c: noop }));           // c: undefined
-    check(c.findOne({ a: "abc", b: noop })); // c: undefined
-    check(c.findOne({ b: noop, a: "abc" })); // c: undefined
+    check(c.find({}).fetch());               // 1 both
+    check(c.findOne({}));                    // 2 first
+    check(c.findOne({ a: noop }));           // 3 undefined
+    check(c.findOne({ b: noop }));           // 4 undefined
+    check(c.findOne({ c: noop }));           // 5 undefined
+    check(c.findOne({ a: "abc", b: noop })); // 6 undefined
+    check(c.findOne({ b: noop, a: "abc" })); // 7 undefined
 
-    check(c.find({ a: noop }).fetch());           // c: []
-    check(c.find({ b: noop }).fetch());           // c: []
-    check(c.find({ c: noop }).fetch());           // c: []
-    check(c.find({ a: "abc", b: noop }).fetch()); // c: []
-    check(c.find({ b: noop, a: "abc" }).fetch()); // c: []
+    check(c.find({ a: noop }).fetch());           // 8  []
+    check(c.find({ b: noop }).fetch());           // 9  []
+    check(c.find({ c: noop }).fetch());           // 10 []
+    check(c.find({ a: "abc", b: noop }).fetch()); // 11 []
+    check(c.find({ b: noop, a: "abc" }).fetch()); // 12 []
   }, 300);
 }
